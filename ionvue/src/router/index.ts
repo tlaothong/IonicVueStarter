9d760899d0ef3b665e8216467ff63440d49575e7
Teerachai Laothong <tlaothong@gmail.com>
2022-08-14 02:14:27 +0700

    Using hash mode in router

--- ionvue/src/router/index.ts
+++ ionvue/src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from '@ionic/vue-router';
+import { createRouter, createWebHashHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
 
 const routes: Array<RouteRecordRaw> = [
@@ -18,7 +18,7 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes
 })
 
