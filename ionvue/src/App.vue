497a843ef080fd97558591efc1bf920885aeaba9
Teerachai Laothong <tlaothong@gmail.com>
2022-08-13 15:36:12 +0700

    Added MyForm.vue to proj

--- ionvue/src/App.vue
+++ ionvue/src/App.vue
@@ -34,7 +34,7 @@
 import { IonApp, IonContent, IonIcon, IonItem, IonLabel, IonList, IonListHeader, IonMenu, IonMenuToggle, IonNote, IonRouterOutlet, IonSplitPane } from '@ionic/vue';
 import { defineComponent, ref } from 'vue';
 import { useRoute } from 'vue-router';
-import { archiveOutline, archiveSharp, bookmarkOutline, bookmarkSharp, heartOutline, heartSharp, mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, trashOutline, trashSharp, warningOutline, warningSharp } from 'ionicons/icons';
+import { briefcase, archiveOutline, archiveSharp, bookmarkOutline, bookmarkSharp, heartOutline, heartSharp, mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, trashOutline, trashSharp, warningOutline, warningSharp } from 'ionicons/icons';
 
 export default defineComponent({
   name: 'App',
@@ -55,6 +55,12 @@ export default defineComponent({
   setup() {
     const selectedIndex = ref(0);
     const appPages = [
+      {
+        title: 'My Form',
+        url: '/myform',
+        iosIcon: briefcase,
+        mdIcon: briefcase
+      },
       {
         title: 'Inbox',
         url: '/folder/Inbox',
--- ionvue/src/router/index.ts
+++ ionvue/src/router/index.ts
@@ -4,11 +4,16 @@ import { RouteRecordRaw } from 'vue-router';
 const routes: Array<RouteRecordRaw> = [
   {
     path: '',
-    redirect: '/folder/Inbox'
+    // redirect: '/folder/Inbox'
+    redirect: '/myform'
   },
   {
     path: '/folder/:id',
     component: () => import ('../views/FolderPage.vue')
+  },
+  {
+    path: '/myform',
+    component: () => import ('../views/MyForm.vue')
   }
 ]
 
